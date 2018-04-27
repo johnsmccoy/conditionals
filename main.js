53b9d759395149a34bce11306cbf6edd9050fd5b
johnsmccoy <johnsmccoy@comcast.net>
2018-04-27 12:57:41 -0500

    completed and corrected work and readme added

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ if (lastName.legnth >=7){
 {console.log("The last name is",)
 
 }
+const firstName = 'Sterling';
+if (firstName.legnth >=7){
+    console.log("i can't pronuce that last name");
+}
+    esle
+{console.log("the last name is",)
+
+}
